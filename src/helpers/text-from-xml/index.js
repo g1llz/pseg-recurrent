b1e7109137364a3e664e5d4d47bb124a5b161cc8
Gilles Lima <gillestoim@gmail.com>
2019-05-10 13:48:38 -0300

    create helpers to remove text attribute from XML

---
+++ src/helpers/text-from-xml/index.js
@@ -0,0 +1,24 @@
+// third party code to remove _text attribute in a xml to json convertion
+const removeJsonTextAttribute = (value, parentElement) => {
+  try {
+    const keyNo = Object.keys(parentElement._parent).length;
+    const keyName = Object.keys(parentElement._parent)[keyNo - 1];
+    parentElement._parent[keyName] = value;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const xml2Opt = {
+  compact: true,
+  trim: true,
+  ignoreDeclaration: true,
+  ignoreInstruction: true,
+  ignoreAttributes: true,
+  ignoreComment: true,
+  ignoreCdata: true,
+  ignoreDoctype: true,
+  textFn: removeJsonTextAttribute
+}
+
+module.exports = xml2Opt;
