ef1f602697a826c32b5f44c0e526a6325c29e587
Gilles Lima <gillestoim@gmail.com>
2019-05-30 11:54:32 -0300

    add notificationDetail

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ const Pagseguro = (auth) => {
   const orders = require('./order')({ auth, ...deps });
   const session = require('./session')({ auth, ...deps });
   const subscription = require('./subscription')({ auth, ...deps });
+  const notification = require('./notification')({ auth, ...deps });
 
   return {
     sessionId: () => session.start(),
     ordersByApprovalCode: code => orders.byApprovalCode(code),
     setDiscountOnNextOrder: discount => orders.discountOnNextOrder(discount),
-    subscriptionByDateInterval: search => subscription.byDateInterval(search) 
+    subscriptionByDateInterval: search => subscription.byDateInterval(search),
+    notificationDetail: (code, type) => notification.approvalOrTransaction(code, type) 
   };
 };
 
