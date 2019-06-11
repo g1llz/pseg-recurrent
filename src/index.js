d1fc39e45a480962af0a2287a7db8bc723fc29b3
Gilles Lima <gillestoim@gmail.com>
2019-06-11 11:33:34 -0300

    update entry point

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 const deps = require('./dependences');
+const orders = require('./pagseguro/order');
+const session = require('./pagseguro/session');
+const subscription = require('./pagseguro/subscription');
+const notification = require('./pagseguro/notification');
 
 /* main const */
-const Pagseguro = (auth) => {
-  const orders = require('./order')({ auth, ...deps });
-  const session = require('./session')({ auth, ...deps });
-  const subscription = require('./subscription')({ auth, ...deps });
-  const notification = require('./notification')({ auth, ...deps });
-
-  return {
-    sessionId: () => session.start(),
-    ordersByApprovalCode: code => orders.byApprovalCode(code),
-    setDiscountOnNextOrder: discount => orders.discountOnNextOrder(discount),
-    subscriptionByDateInterval: search => subscription.byDateInterval(search),
-    notificationDetail: (code, type) => notification.approvalOrTransaction(code, type) 
-  };
+const Pagseguro = function(auth) {
+  this.config = { auth, ...deps };
 };
+
+Pagseguro.prototype.sessionId = () => session.start()(this.config);
+
+Pagseguro.prototype.ordersByApprovalCode = (code) => 
+  orders.byApprovalCode(code)(this.config); 
+
+Pagseguro.prototype.setDiscountOnNextOrder = (discount) =>
+  orders.discountOnNextOrder(discount)(this.config);
+
+Pagseguro.prototype.subscriptionByDateInterval = (search) =>
+  subscription.byDateInterval(search)(this.config);
+
+Pagseguro.prototype.notificationDetail = (code, type) =>
+  notification.approvalOrTransaction(code, type)(this.config);
 
 module.exports = Pagseguro;
